4b795408d0ab212849f53f2931d3176b66c08203
cleberro@hotmail.com <cleberro@hotmail.com>
2021-02-22 18:34:17 -0300

    atualização 20210222

---
+++ aulas/primeiro-projeto-node/src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,37 @@
+/* eslint-disable arrow-parens */
+/* eslint-disable function-paren-newline */
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable prettier/prettier */
+/* eslint-disable indent */
+import { isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
+
+class AppointmentsRepository {
+  private appointments: Appointment[];
+
+  constructor() {
+    this.appointments = [];
+  }
+
+  public all(): Appointment[] {
+    return this.appointments;
+  }
+
+  public findByDate(date: Date): Appointment | null {
+    const findAppointment = this.appointments.find(appointment =>
+      isEqual(date, appointment.date),
+    );
+
+    return findAppointment || null;
+  }
+
+  public create(provider: string, date: Date): Appointment {
+    const appointment = new Appointment(provider, date);
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+}
+
+export default AppointmentsRepository;
